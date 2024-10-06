8eba1ebda085b5f7b5dc7c72b82ae9fd155811be
Elza <elza.assanova@gmail.com>
2024-10-06 21:27:17 +0600

    first commit

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -6,8 +6,5 @@ public class Main {
     public static void main(String[] args) {
         Project project = new Project();
 
-        System.out.println(project.calculateDigits("Hello123")); // Output: 3
-        System.out.println(project.calculateDigits("No digits here!")); // Output: 0
-        System.out.println(project.calculateDigits("Digits: 4567 and 89")); // Output: 6
     }
 }
